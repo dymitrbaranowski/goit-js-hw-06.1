941ae40b01128db8e596c4c9ceacb786e71a5460
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-09-18 21:19:14 +0300

    Update.Task-7

---
+++ js/task-07.js
@@ -0,0 +1,13 @@
+const input = document.querySelector("#font-size-control");
+const span = document.querySelector("#text");
+// console.log((span.style.fontSize = "24px"));
+
+// console.log(input);
+input.addEventListener("input", onInputChange);
+
+function onInputChange(evt) {
+  //console.log(evt.currentTarget.value);
+  const valueEl = evt.currentTarget.value;
+  //   console.log(valueEl);
+  span.style.fontSize = `${valueEl}px`;
+}
