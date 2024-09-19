761721908d67b5a83b1ee7ca80323e8edab94fb1
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-09-19 20:22:20 +0300

    fox4

--- js/task-10.js
+++ js/task-10.js
@@ -6,8 +6,8 @@ const refs = {
   boxes: document.querySelector("#boxes"),
 };
 
-// refs.createrBtn.addEventListener("click", createBoxes);
-refs.destroyerBtn.addEventListener("click", destroyBoxes);
+//refs.createrBtn.addEventListener("click", createBoxes);
+// refs.destroyerBtn.addEventListener("click", destroyBoxes);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -18,33 +18,60 @@ refs.inputNum.addEventListener("change", onInputAmount);
 console.log(refs.inputNum);
 
 function onInputAmount(evt) {
-  console.log(evt.target);
-  const amount = evt.currentTarget.valueAsNumber;
-  // console.log(amount);
+  const amount = Number(evt.target.value);
+  console.log(amount);
   refs.createrBtn.addEventListener("click", createBoxes);
-  // createBoxes(amount);
+  function createBoxes(amount) {
+    console.log("This is createBoxes");
+    const amountEl = amount;
+    console.log(amountEl);
+    const boxesMarkup = [];
+    for (let i = 0; i < amountEl; i += 1) {
+      const div = document.createElement("div");
+      div.style.height = `${30 + 10 * i}px`;
+      div.style.width = `${30 + 10 * i}px`;
+      div.style.backgroundColor = getRandomHexColor();
+      boxesMarkup.push(div);
+    }
+    // console.log(amount);
+    console.log(boxesMarkup);
+
+    refs.boxes.append(...boxesMarkup);
+  }
 }
 // console.log(amount);
 
-function createBoxes(amount) {
-  onInputAmount();
-  console.log("This is createBoxes");
-  console.log(amount);
+// function createBoxes(amount) {
+//   onInputAmount();
+//   console.log("This is createBoxes");
+//   console.log(amount);
 
-  const boxesMarkup = [];
-  for (let i = 0; i < amount; i += 1) {
-    const div = document.createElement("div");
-    div.style.height = `${30 + 10 * i}px`;
-    div.style.width = `${30 + 10 * i}px`;
-    div.style.backgroundColor = getRandomHexColor();
-    boxesMarkup.push(div);
-  }
+//   const boxesMarkup = [];
+//   for (let i = 0; i < amount; i += 1) {
+//     const div = document.createElement("div");
+//     div.style.height = `${30 + 10 * i}px`;
+//     div.style.width = `${30 + 10 * i}px`;
+//     div.style.backgroundColor = getRandomHexColor();
+//     boxesMarkup.push(div);
+//   }
 
-  // console.log(boxesMarkup);
+// console.log(boxesMarkup);
 
-  refs.boxes.append(...boxesMarkup);
-}
-function destroyBoxes() {
-  console.log("This is destroyBoxes");
-  // refs.boxes;
-}
+//   refs.boxes.append(...boxesMarkup);
+// }
+// function destroyBoxes() {
+//   console.log("This is destroyBoxes");
+//   // refs.boxes;
+// }
+
+// for (let i = 1; i < amountEl; i += 1) {
+//   const div = document.createElement("div");
+//   div.style.height = `${30 + 10 * i}px`;
+//   div.style.width = `${30 + 10 * i}px`;
+//   div.style.backgroundColor = getRandomHexColor();
+//   boxesMarkup.push(div);
+// }
+// // console.log(amount);
+// console.log(boxesMarkup);
+
+// refs.boxes.append(...boxesMarkup);
