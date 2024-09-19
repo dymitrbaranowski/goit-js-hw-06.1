b7597cf26db4c2f0a3897b642b4d48f0bab68da3
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-09-19 14:56:44 +0300

    fox2

--- js/task-09.js
+++ js/task-09.js
@@ -1,19 +1,21 @@
 const refs = {
-  container: document.querySelector(".widget"),
+  // container: document.querySelector(".widget"),
   span: document.querySelector(".color"),
   button: document.querySelector(".change-color"),
 };
 
 refs.button.addEventListener("click", onChangeColor);
 
-function onChangeColor(getRandomHexColor) {
-  // console.log("This is onChangeColor");
-  console.dir(document.body);
-  document.body.style.backgroundColor = "getRandomHexColor()";
-}
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+function onChangeColor() {
+  // console.log("This is onChangeColor");
+  // console.dir(document.body);
+  let color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  refs.span.textContent = color;
 }
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,12 @@
+const refs = {
+  container: document.querySelector("#controls"),
+  boxes: document.querySelector("#boxes"),
+};
+
+const genValue = refs.container.firstElementChild;
+// console.log(genValue);
+
+genValue.addEventListener("");
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
